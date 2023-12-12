42307f9d6286b0d65c78e8d7e4620c35e4f9ffd9
Essalhi Adam <200537@CCM-Asty-Moulin.local>
2023-12-12 15:09:22 +0100

    examFin

---
+++ CeUAA14Partie2_dec23_ESSALHIADAM/MesMethodes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeUAA14Partie2_dec23_ESSALHIADAM
+{
+    public class MesMethodes
+    {
+        public void TirerArme()
+        { 
+        
+        }
+        public void RechargementArme()
+        {
+
+        }
+        public void VideOuPas()
+        {
+            
+        }
+    }
+}
---
+++ CeUAA14Partie2_dec23_ESSALHIADAM/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CeUAA14Partie2_dec23_ESSALHIADAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue dans ce jeu de tir ... Vous démarrez avec 30 balles");
+            Console.WriteLine("=============================================================");
+
+            Console.WriteLine("Espace pour tirer,");
+            Console.WriteLine("r pour recharger,");
+            Console.WriteLine("+ pour reprendre des munitions,");
+            Console.WriteLine("q pour quitter,");
+
+
+        }
+    }
+}
